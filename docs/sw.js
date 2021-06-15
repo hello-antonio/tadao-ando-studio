5616de49dca88ea1c711758d72d4f7ed84d79c7b
Giovanni Antonio <hello.giolara@gmail.com>
2021-06-15 14:43:59 -0700

    latest

--- docs/sw.js
+++ docs/sw.js
@@ -1,7 +1,5 @@
-// https://github.com/sarmis/greekdev-template-vue-spa/blob/master/src/web/service-worker.js
-// import service worker script
 importScripts(
-  "https://storage.googleapis.com/workbox-cdn/releases/4.2.0/workbox-sw.js"
+  "https://storage.googleapis.com/workbox-cdn/releases/6.1.5/workbox-sw.js"
 );
 
 // Network First
